fda2c5e9aa38446b567f02eb3604766b038918e8
Anh Lu <anhlucci@outlook.com>
2018-06-09 10:02:33 -0400

    some extra changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,20 @@
 var phrase = ["appletini", "coding", "javascript"];
-var rand = Math.floor(Math.random()*phrase.length);
-var word = phrase[rand];
+
 var underScore = [];
 var correctGuess = [];
 var incorrectGuess = [];
+var guessesRemaining = 10;
+var wins = 0;
+var wonTheRound = false;
 
 
+
+var generateWord = function(){
+    rand = Math.floor(Math.random()*phrase.length);
+    word = phrase[rand];
+}
+generateWord();
 console.log(word);
-
 // To create underscores
 var generateUnderscore = function () {
     for(var i=0; i<word.length; i++){
@@ -16,15 +23,29 @@ var generateUnderscore = function () {
     return underScore;
 };
 
-// 
-// 
+var reset = function(){
+    guessesRemaining = 10;
+    generateWord();
+    underScore = [];
+    correctGuess = [];
+    incorrectGuess = [];
+    generateUnderscore();
+    wonTheRound = false;
+}
+
+// Run function Reset
 
 // To display underscores
 document.addEventListener('keyup', function(event){
     generateUnderscore().length = word.length;
     document.getElementById("currentWord").innerHTML = underScore.join(' ');
-    if (underScore.join('') == word.toUpperCase()){
-        // Need to add wins
+
+    // if (underScore.join('') > word.toUpperCase()){
+    //     $("span").empty();
+    
+    if (underScore.join('') == word.toUpperCase() && wonTheRound === false){
+        document.getElementById("wins").innerHTML = ++wins;
+        wonTheRound = true;
     }
 })
 
@@ -32,11 +53,18 @@ document.addEventListener('keyup', function(event){
 
 document.addEventListener('keydown', function(event){
     var keyWord = String.fromCharCode(event.keyCode)
+
+    if (wonTheRound === false){
+    guessesRemaining--};
+
+    document.getElementById("lives").innerHTML = guessesRemaining;
     for ( var i = 0; i < word.length; i++){
     if(word[i] == keyWord.toLowerCase()) {
         // correctGuess.push(keyWord);
         underScore[i] = keyWord;
     }
+    
+    
     } 
     
     // else {
