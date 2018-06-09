701aae756d9c7cacb2ce8d2999ec2713f99688f0
Anh Lu <anhlucci@outlook.com>
2018-06-08 20:42:51 -0400

    some changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,14 @@
-var phrase = ["appletini", "bud light", "cryptocurrency & blockchain", "dwarves", "zombie apocalypse"];
+var phrase = ["appletini", "coding", "javascript"];
 var rand = Math.floor(Math.random()*phrase.length);
 var word = phrase[rand];
-var win = 0;
-var phraseLength = 0;
-var maxErrors = 15;
 var underScore = [];
+var correctGuess = [];
+var incorrectGuess = [];
 
+
+console.log(word);
+
+// To create underscores
 var generateUnderscore = function () {
     for(var i=0; i<word.length; i++){
         underScore.push('_');
@@ -13,7 +16,32 @@ var generateUnderscore = function () {
     return underScore;
 };
 
+// 
+// 
 
+// To display underscores
 document.addEventListener('keyup', function(event){
-    document.getElementById("currentWord").textContent = generateUnderscore;
+    generateUnderscore().length = word.length;
+    document.getElementById("currentWord").innerHTML = underScore.join(' ');
+    if (underScore.join('') == word.toUpperCase()){
+        // Need to add wins
+    }
 })
+
+// To capture users' guess
+
+document.addEventListener('keydown', function(event){
+    var keyWord = String.fromCharCode(event.keyCode)
+    for ( var i = 0; i < word.length; i++){
+    if(word[i] == keyWord.toLowerCase()) {
+        // correctGuess.push(keyWord);
+        underScore[i] = keyWord;
+    }
+    } 
+    
+    // else {
+    //     incorrectGuess.push(keyWord);
+    // }
+})
+
+
